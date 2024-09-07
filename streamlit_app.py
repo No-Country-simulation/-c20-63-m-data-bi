d94c76ff2f8296bb83d94b802d919012f79a4b78
Sergius-DS <sergioskhy@gmail.com>
2024-09-07 21:37:59 +0000

    updating app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("Hello DS")
 
 st.info("This app builds a ML model")
 # Get the raw GitHub URL for the file
-url = "https://raw.githubusercontent.com/No-Country-simulation/-c20-63-m-data-bi/main/carrito.csv?"
+url = "https://raw.githubusercontent.com/No-Country-simulation/-c20-63-m-data-bi/main/carrito.csv?token=GHSAT0AAAAAACUFGUDGAY2LLEBLLJEE74FMZW4Y6TA"
 
 # Read the Excel file into a pandas DataFrame
 df = pd.read_csv(url)
