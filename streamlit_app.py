d948bc58dd47b1b4f0a4ba16d6cdccf2eeeea2f2
Sergius-DS <sergioskhy@gmail.com>
2024-09-07 20:15:36 +0000

    adding df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
-
-
+import pandas as pd
 st.title("Hello DS")
 
 st.info("This app builds a ML model")
+# Get the raw GitHub URL for the file
+url = "https://raw.githubusercontent.com/No-Country-simulation/-c20-63-m-data-bi/main/E-commerce.xlsx"
+
+# Read the Excel file into a pandas DataFrame
+df = pd.read_excel(url)
+df
