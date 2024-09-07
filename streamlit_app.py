ecee68a7d683d4efbc133415f237374e517aa945
Sergius-DS <sergioskhy@gmail.com>
2024-09-07 21:56:05 +0000

    updating app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 from github import Github
 import io
-
+import base64
 
 # Assuming you have a GitHub token stored securely, not hardcoded
 g = Github("GHSAT0AAAAAACUFGUDHWTOAQOLVP23Z67C2ZW4ZJSQ")
@@ -12,14 +12,13 @@ repo = g.get_repo("No-Country-simulation/-c20-63-m-data-bi")
 contents = repo.get_contents("main/carrito.csv")
 
 # Decode the content if it's base64 encoded
-import base64
+
 file_content = base64.b64decode(contents.content).decode()
+df = pd.read_csv(io.StringIO(file_content))
 
 st.title("Hello DS")
 
 st.info("This app builds a ML model")
-
-df = pd.read_csv(io.StringIO(file_content))
 df
 
 
